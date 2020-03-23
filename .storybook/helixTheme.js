6348a970a2d0aae363fd4abdf47ab18ca9868338
Nicko Winner-Arroyo <nicko.winner-arroyo@rackspace.com>
2020-03-23 16:36:05 -0400

    cleanup

--- .storybook/helixTheme.js
+++ .storybook/helixTheme.js
@@ -2,8 +2,6 @@ import { create } from '@storybook/theming/create';
 
 export default create({
     base: 'light',
-
     brandTitle: 'Helix React',
     brandUrl: 'https://github.com/HelixDesignSystem/helix-ui',
-    brandImage: 'https://placehold.it/350x150',
 });
