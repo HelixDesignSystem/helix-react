6d2fa8ec49f47ef249784bf463ead5359f7cabea
Nicko Winner-Arroyo <nicko.winner-arroyo@rackspace.com>
2020-03-23 16:31:00 -0400

    Use the new Theme syntax to replace branding settings that used to be set in options
    https://storybook.js.org/docs/configurations/theming/

---
+++ .storybook/helixTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+
+    brandTitle: 'Helix React',
+    brandUrl: 'https://github.com/HelixDesignSystem/helix-ui',
+    brandImage: 'https://placehold.it/350x150',
+});
