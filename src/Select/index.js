eac648f704c82badf15a58f32db813dd4907892e
Mike Mangialardi <mike.mangialardi@rackspace.com>
2020-04-03 13:57:07 -0400

    Create Select component

---
+++ src/Select/index.js
@@ -0,0 +1,56 @@
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Select = ({
+  children,
+  disabled,
+  id,
+  label,
+  onChange,
+  optional,
+  required,
+}) => {
+  return (
+    <hx-select-control>
+      <select
+        id={id}
+        disabled={disabled}
+        onChange={onChange}
+        required={required}
+      >
+        {children}
+      </select>
+      <hx-select></hx-select>
+      {!!label && (
+        <label
+          className={classnames({
+            hxOptional: optional,
+            hxRequired: required,
+          })}
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      )}
+    </hx-select-control>
+  );
+};
+
+Select.propTypes = {
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  optional: PropTypes.bool,
+  required: PropTypes.bool,
+};
+
+Select.defaultProps = {
+  disabled: false,
+  optional: false,
+  required: false,
+};
+
+export default Select;
---
+++ src/Select/stories.js
@@ -0,0 +1,51 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import Select from '../Select';
+
+storiesOf('Select', module)
+.add('Basic', () => {
+  return (
+    <Demo />
+  );
+})
+.add('Disabled', () => {
+  return (
+    <Demo disabled />
+  );
+})
+.add('Required', () => {
+  return (
+    <Demo required />
+  );
+})
+.add('Optional', () => {
+  return (
+    <Demo optional />
+  );
+})
+
+const Demo = (props) => {
+  return (
+    <div style={{ padding: 25, width: 500 }}>
+      <Select
+        label="Select"
+        onChange={({ target: { value }}) => console.log(`changed to ${value}`)}
+        {...props}
+      >
+        <Options />
+      </Select>
+    </div>
+  );
+};
+
+const Options = () => {
+  return (
+    <React.Fragment>
+      <option value="">--Please choose an option--</option>
+      <option value="derp">Some option</option>
+      <option value="chirp">Another option</option>
+      <option value="slurp">Third option</option>
+    </React.Fragment>
+  );
+};
