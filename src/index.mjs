ce651923f5c4c53aaf639edb84f46c49eec4175a
Nicko Winner-Arroyo <nicko.winner-arroyo@rackspace.com>
2020-04-10 19:24:15 -0400

    Alert Component

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,10 @@
 /* Export helix-react definition */
 import Button from './Button';
+import Alert from './Alert';
 import Icon from './Icon';
 
 export default {
     Button,
     Icon,
+    Alert
 };
