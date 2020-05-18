a8791875fb0325d6d50d9972c67dd18776f66669
Mike Mangialardi <mike.mangialardi@rackspace.com>
2020-05-18 17:04:45 -0400

    Implement choice tile

---
+++ src/Choice Tile/index.js
@@ -0,0 +1,65 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { SIZES } from '../constants';
+import { wcBool } from '../utils';
+
+const ChoiceTile = ({
+  checked,
+  children,
+  className,
+  disabled,
+  icon,
+  id,
+  invalid,
+  onChange,
+  size,
+  subdued,
+  title,
+  ...rest
+}) => {
+  return (
+    <label className="hxChoice">
+      <input
+        checked={wcBool(checked)}
+        disabled={wcBool(disabled)}
+        invalid={wcBool(invalid)?.toString()}
+        name={id}
+        type="radio"
+      />
+      <hx-tile
+        class={classNames({ className, hxSubdued: wcBool(subdued), [SIZES[size]]: true })}
+        {...rest}
+      >
+        <hx-icon type="checkmark"></hx-icon>
+        {icon && (
+          <div className="hx-tile-icon">
+            <hx-icon type={icon}></hx-icon>
+          </div>
+        )}
+        <header>{title}</header>
+        {children}
+      </hx-tile>
+    </label>
+  );
+};
+
+ChoiceTile.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  icon: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  invalid: PropTypes.bool,
+  size: PropTypes.oneOf(SIZES),
+  subdued: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+};
+
+ChoiceTile.defaultProps = {
+  checked: false,
+};
+
+export default ChoiceTile;
