8b005c64f1abf859c7ee59eecf3f658e92dd6b90
Nicko Winner-Arroyo <nicko.winner-arroyo@rackspace.com>
2020-05-26 11:50:56 -0400

    Review Fixes

--- src/Disclosure/index.js
+++ src/Disclosure/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { wcBool } from '../utils';
 
 const Disclosure = ({ className, ariaControls, ariaExpanded, ...rest }) => {
   return (
     <hx-disclosure
       class={className}
       aria-controls={ariaControls}
-      aria-expanded={ariaExpanded}
+      aria-expanded={wcBool(ariaExpanded)}
       {...rest}
     />
   );
 };
 
 Disclosure.propTypes = {
-  ariaControls: PropTypes.string,
-  ariaExpanded: PropTypes.string,
+  ariaControls: PropTypes.string.isRequired,
+  ariaExpanded: PropTypes.bool,
   role: PropTypes.string,
   tabindex: PropTypes.string,
   className: PropTypes.string,
--- src/Popover/index.js
+++ src/Popover/index.js
@@ -5,20 +5,21 @@ import useEventListener from '../hooks/useEventListener';
 import { POSITIONS } from '../constants';
 import { wcBool } from '../utils';
 
-const Popover = ({ onOpen, onClose, onReposition, className, open, children, ...rest }) => {
+const Popover = ({ onOpen, onClose, onReposition, className, open, children, relativeTo, ...rest }) => {
   const hxRef = useEventListener({ onOpen, onClose, onReposition });
   return (
-    <hx-popover class={classNames(className)} ref={hxRef} open={wcBool(open)} {...rest}>
+    <hx-popover class={classNames(className)} ref={hxRef} open={wcBool(open)} relative-to={relativeTo} {...rest}>
       {children}
     </hx-popover>
   );
 };
 
 Popover.propTypes = {
-  id: PropTypes.string,
-  position: PropTypes.oneOf(POSITIONS),
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  id: PropTypes.string,
+  position: PropTypes.oneOf(POSITIONS),
+  relativeTo: PropTypes.string,
   open: PropTypes.bool,
   onClose: PropTypes.func,
   onOpen: PropTypes.func,
