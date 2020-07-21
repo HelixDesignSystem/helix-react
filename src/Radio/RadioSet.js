09b34a6ae8ea6349cd38942607e594caa59b2ea4
Nicko Winner-Arroyo <nicko.winner-arroyo@rackspace.com>
2020-07-20 22:16:14 -0400

    add stories

--- src/Radio/RadioSet.js
+++ src/Radio/RadioSet.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const RadioSet = ({ className, children, ...rest}) => {
+const RadioSet = ({ className, children, ...rest }) => {
   return (
     <hx-radio-set class={className} {...rest}>
       {children}
--- src/Radio/stories.js
+++ src/Radio/stories.js
@@ -6,46 +6,48 @@ import { storiesOf } from '@storybook/react';
 import Radio from './index';
 import RadioSet from './RadioSet';
 
-storiesOf('Radio', module).add('All Knobs', () => {
-  let label = text('label', 'Radio Label');
-  let checked = boolean('checked', false);
-  let disabled = boolean('disabled', false);
-  let required = boolean('required', false);
-  return (
-    <Radio
-      id="radioDemo"
-      {...(checked && { checked })}
-      {...(disabled && { disabled })}
-      {...(required && { required })}
-      {...(label && { label })}
-      onChange={action('onChange')}
-    />
-  );
-}).add('Radio Set', () => {
+storiesOf('Radio', module)
+  .add('All Knobs', () => {
+    let label = text('label', 'Radio Label');
+    let checked = boolean('checked', false);
+    let disabled = boolean('disabled', false);
+    let required = boolean('required', false);
+    return (
+      <Radio
+        id="radioDemo"
+        {...(checked && { checked })}
+        {...(disabled && { disabled })}
+        {...(required && { required })}
+        {...(label && { label })}
+        onChange={action('onChange')}
+      />
+    );
+  })
+  .add('Radio Set', () => {
     let checked = boolean('default checked', false);
     let disabled = boolean('disabled', false);
     let required = boolean('required', false);
     return (
-        <RadioSet>
-            <Radio
-                id="radioDemo1"
-                name="myRadios"
-                value="option1"
-                label="option 1"
-                {...(checked && { checked })}
-                {...(disabled && { disabled })}
-                {...(required && { required })}
-                onChange={action('onChange')}
-            />
-            <Radio
-                id="radioDemo2"
-                name="myRadios"
-                value="option2"
-                label="option 2"
-                {...(disabled && { disabled })}
-                {...(required && { required })}
-                onChange={action('onChange')}
-            />
-        </RadioSet>
+      <RadioSet>
+        <Radio
+          id="radioDemo1"
+          name="myRadios"
+          value="option1"
+          label="option 1"
+          {...(checked && { checked })}
+          {...(disabled && { disabled })}
+          {...(required && { required })}
+          onChange={action('onChange')}
+        />
+        <Radio
+          id="radioDemo2"
+          name="myRadios"
+          value="option2"
+          label="option 2"
+          {...(disabled && { disabled })}
+          {...(required && { required })}
+          onChange={action('onChange')}
+        />
+      </RadioSet>
     );
-});
+  });
