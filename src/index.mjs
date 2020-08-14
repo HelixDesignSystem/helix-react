380e839c21f4fa599b14a9e515c36b47c06866d7
Nicko Winner-Arroyo <nicko.winner-arroyo@rackspace.com>
2020-08-14 15:46:31 -0400

    make sure all imports are included

--- src/index.mjs
+++ src/index.mjs
@@ -3,35 +3,44 @@ import Alert from './Alert';
 import Button from './Button';
 import Checkbox from './Checkbox';
 import ChoiceTile from './ChoiceTile';
+import Disclosure from './Disclosure';
+import Div from './Div';
 import Drawer from './Drawer';
 import Icon from './Icon';
 import Modal from './Modal';
 import Pill from './Pill';
 import Status from './Pill/Status';
 import Popover from './Popover';
-import Tooltip from './Tooltip';
 import Radio from './Radio';
 import RadioSet from './Radio/RadioSet';
 import Search from './Search';
 import SearchAssist from './Search/SearchAssist';
 import Select from './Select';
-
+import Switch from './Switch';
+import Text from './Text';
+import TextArea from './TextArea';
+import Tooltip from './Tooltip';
 
 export default {
   Alert,
   Button,
   Checkbox,
   ChoiceTile,
+  Disclosure,
+  Div,
   Drawer,
   Icon,
   Modal,
   Pill,
   Popover,
-  Tooltip,
   Radio,
   RadioSet,
   Search,
   Status,
   SearchAssist,
   Select,
+  Switch,
+  Text,
+  TextArea,
+  Tooltip
 };
